9f8e800787c76dc555f3c4974e3c96ecabf1a560
leoogomesz <lgomes@tecnologia-unica.com.br>
2023-02-15 21:01:03 -0300

    first commit orgs-cesta-app

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/telas/Cesta/Index';
+import mock from './src/mocks/cesta';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import montserrat from '@expo-google-fonts/montserrat';
+import { useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular":  Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+  if(!fonteCarregada){
+    return <AppLoading/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <StatusBar/>
+      <Cesta {...mock}/>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
